51139a439411c79288f82fd7bb2bf72df0ec55a7
AontoreAontore <80526952+AontoreAontore@users.noreply.github.com>
2021-05-12 22:04:04 -0300

    Add files via upload

---
+++ JAVA CODES/AO1/WhoAmI.java
@@ -0,0 +1,50 @@
+
+/**
+ *A program called Who Am I 
+ * @author MD ISHFAQ ALAM (A00450249)
+ */
+public class WhoAmI {
+
+    /** 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /**
+         * Writing out names using * character 
+         */
+        System.out.println();
+        System.out.println("\t" + "Who Am I " + "\n" 
+                +"\t" + "--------" + "\n");
+        System.out.println("CSCI 1228 -- Winter 2021" + "\n");
+        System.out.println("*   *" + "\t" + "**** " + "\t  " + "*****" + "\t " 
+                +  " ****" + "\t " + "*   *" + "\t " 
+                + "*****" + "\t " + "*" + "     " + " ***"); 
+        System.out.println("** **" + "\t" + "*   *" + "\t  " 
+                + "  *" + "\t " + "*" + "\t " + "*   *" + "\t " 
+                + "*" + "\t" + "* *" + "    " + "*   *");
+        System.out.println("* * *" + "\t" + "*   *" + "\t  " + "  *" 
+                + "\t " + " *** " + "\t " + "*****" + "\t " 
+                + "***"  + "   *****" + "   " + "*   *");
+        System.out.println("*   *" + "\t" + "*   *" + "\t  " + "  *" + "\t " 
+                + "    *" + "\t " + "*   *" + "\t " 
+                + "*" + "     *   *" + "   " + "*  *");
+        System.out.println("*   *" + "\t" + "****" + "\t  " + "*****" 
+                + "\t " + "**** " + "\t " + "*   *" + "\t " 
+                + "*"  + "     *   *" + "   " + " ** *");
+        
+        /**
+         * Gap in 12th row
+         */
+        System.out.print("\n");
+        
+        /**
+         * Printing Name and ID
+         */
+        System.out.println("  " + "A00450249: Md Ishfaq Alam");
+        
+        
+        System.out.println("\n");
+    }  
+
+}
